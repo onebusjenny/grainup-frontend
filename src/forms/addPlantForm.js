251050019713e6920730e7345fd7523cd7bbe01c
Jenny <jchudesign0930@gmail.com>
2020-09-15 02:35:55 -0400

    added addPlantForm form

--- src/forms/addPlantForm.js
+++ src/forms/addPlantForm.js
@@ -1,6 +1,49 @@
 class addPlantForm extends React.Component{
-    render(){
+    constructor(){
+        super()
+        this.state = {
+            name: '',
+            amount: '',
+            date: ''
+        }
+    }
 
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+    }
+
+
+    render(){
+        <form onSubmit={this.handleSubmit}>
+            <div>
+                <label>
+                    Goal Name:
+                    <input id="goal_name" name="goal_name" type= "text" onChange={this.handleChange} value={this.state.name}></input>
+                </label>
+            </div>
+            <div>
+                <label>
+                    Amount:
+                    <input id="goal_amount" name="goal_amount" type="text" onChange={this.handleChange} value={this.state.amount}></input>
+                </label>
+            </div>
+            <div>
+                <label>
+                    Date:
+                    <input id="goal_date" name="goal_date" tyep="text" onChange={this.handleChange} value={this.state.date}></input>
+                </label>
+            </div>
+            <div>
+                <button type="submit">Create your plant</button>
+            </div>
+        </form>
     }
 }
 
