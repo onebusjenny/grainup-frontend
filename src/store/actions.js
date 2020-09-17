8940c96116ebe116c3a762a4d62c9b56195d3b39
Jenny <jchudesign0930@gmail.com>
2020-09-17 17:02:22 -0400

    added action

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,3 @@
-
-export function createPlant(){
-   
-};
-
 export function fetchPlants(){ 
     return(dispatch) => {
         fetch('http://localhost:3000/api/v1/plants')
@@ -14,10 +9,44 @@ export function fetchPlants(){
 };
 
 export function fetchWaters(){
+    return(dispatch) => {
+        fetch('http://localhost:3000/api/v1/waters')
+        .then(res => 
+            res.json()) 
+        .then(res =>  dispatch({type:'SET_WATERS',waters:res}) 
+
+        )}
 
 };
 
-export function addWater(){
-
+export function addPlant(name,amount,date){
+    return(dispatch) => {
+        return fetch('http://localhost:3000/api/v1/plants',{
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              },
+                method: 'POST',
+                body: JSON.stringify({plant: name,amount,date})
+            })
+              .then(response => response.json())
+              .then(plant => dispatch({type: 'ADD_PLANT', plant: plant})
+              )
+    }
 };
 
+export function addWater(entry){
+    return(dispatch) => {
+        return fetch('http://localhost:3000/api/v1/waters',{
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              },
+                method: 'POST',
+                body: JSON.stringify({water: entry})
+            })
+              .then(response => response.json())
+              .then(water => dispatch({type: 'ADD_WATER', water: water})
+              )
+    }
+};
