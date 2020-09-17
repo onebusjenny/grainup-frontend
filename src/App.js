a95108b4cc551ec47c159f9911d557fb48de0aef
Jenny <jchudesign0930@gmail.com>
2020-09-17 02:38:39 -0400

    added routes, not working yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import {connect} from 'react-redux'
-
 import Home from './pages/home'
-
+import { Router, Route } from 'react-router'
 import { render } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">    
-      
+      <h1>Grain Up | Save Up</h1>
+      <h3>start with planting a goal</h3>
       </header>
       <Home />
-      {/* <Router>
+      <Router>
         <div>
-          <Route exact path ="/" component={home}/>
-          <Route path="/showPlant" render={routerProps=> <showPlant {...routerProps} plants={this.states.plants}/>}/>
+          <Route exact path ="/" component={Home}/>
+          <Route exact path='/plants' render={(...routerprops) => <showPlants plants={this.props.plants} />} />
+          <Route exact path='/plants/new' render={(...routerprops) => <addPlantForm addPlant={this.props.addplant}/>}/>
+          <Route path='/logs/:id' render={(...routerprops) => <showPlant plants={this.props.plants} />} />
         </div>
-      </Router> */}
+      </Router>
     </div>
     
    
---
+++ src/containers/plant_container.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {Connect} from 'react-redux'
+import{Route} from 'react-router-dom'
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchPlants} from '../store/actions'
+//import home.css
+
+class plantContainer extends React.Component{
+
+    componentDidMount(){
+        this.props.fetchPlants()
+        console.log("hi")
+    }
+
+    renderEmptyView(){
+        return (
+            <div>
+                <p>this is the empty garden</p>
+                <button>+</button>
+            </div>
+        )
+    }
+
+    renderPlantView(){
+        const plant = this.props.plants[0]
+        return(
+            <div>
+                <p>{plant.name}</p>
+                <p>${plant.amount}</p>
+                <p>garden with plants</p>
+                <button>Water me</button>
+            </div>
+        )
+    }
+
+    render(){
+        if(this.props.hasPlants){
+            return(
+                this.renderPlantView()
+                )}
+            return (
+            this.renderEmptyView() 
+        )
+        
+    }
+}
+function mapStateToProps(state){
+    return {
+        plants: state.plants,
+        hasPlants: state.plants.length > 0
+
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+        fetchPlants: () => {dispatch(fetchPlants())}
+        
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps) (PlantContainer)
